7e801408db3d1b56745738c9e14ae8dad1601b86
MotacillaAlba <rowena.rowena.lily@gmail.com>
2022-11-09 23:32:24 +0800

    working on the dqn demo

--- demo_rl/demo_dqn.py
+++ demo_rl/demo_dqn.py
@@ -7,59 +7,13 @@ import time
 import numpy as np
 from PIL import Image
 import gym
+import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 
 from rllib.algorithms.dqn import DQN
 
 
 np.random.seed(20)
-
-
-def process_atari(state):
-    im = Image.fromarray(state, mode="RGB")
-    im = im.resize((84, 84))
-    gray = im.convert('L')
-    im_array = np.array(im)
-    gray_array = np.expand_dims(np.array(gray), axis=2)
-    processed_state = np.concatenate((im_array, gray_array), axis=2)
-    processed_state = np.transpose(processed_state, (2, 0, 1))
-    return processed_state
-
-
-def train_atari(
-    env: gym.Env, agent:DQN, num_episode: int, time_step: int, update_freq: int,
-    writer: SummaryWriter
-):
-
-    total_steps = 0
-    # cnt_step = 0
-
-    for episode in range(num_episode):
-        score = 0
-        state = env.reset()
-        for i in range(time_step):
-            processed_state = process_atari(state)
-            action = agent.get_action(processed_state, total_steps)
-            next_state, reward, done, _ = env.step(action)
-            done_mask = 0.0 if done else 1.0
-            agent.buffer.push((state, action, reward, done_mask))
-
-            state = next_state
-            score += reward
-            total_steps += 1
-            # cnt_step += 1
-            if done:
-                break
-            # if cnt_step > update_freq:
-            if len(agent.buffer) > 1000:
-                agent.train()
-                # cnt_step = 0
-
-        print("episode:{}, Return:{}, buffer_capacity:{}".format(episode, score, len(agent.buffer)))
-        writer.add_scalar("score", score, episode)
-        score = 0
-
-    env.close()
 
 
 def tensorboard_writer(env_name):
@@ -73,19 +27,73 @@ def tensorboard_writer(env_name):
     writer = SummaryWriter(writer_path)
     return writer
 
+
+def process_atari(state):
+    im = Image.fromarray(state, mode="RGB")
+    im = im.resize((84, 84))
+    gray = im.convert('L')
+    im_array = np.array(im)
+    gray_array = np.expand_dims(np.array(gray), axis=2)
+    processed_state = np.concatenate((im_array, gray_array), axis=2)
+    processed_state = np.transpose(processed_state, (2, 0, 1))
+    return processed_state
+
+
+def train(
+    env: gym.Env, agent:DQN, num_episode: int, update_freq: int,
+    env_name: str, writer: SummaryWriter, preprocess_state = None
+):
+    total_steps = 0
+    cnt_step = 0
+
+    for episode in range(num_episode):
+        score = 0
+        # duration = 0
+        state = env.reset()
+        is_done = False
+        while not is_done:
+            if preprocess_state is not None:
+                state = preprocess_state(state)
+            action = agent.get_action(state, total_steps)
+            next_state, reward, done, _ = env.step(action)
+            is_done = done
+            done_mask = 0.0 if done else 1.0
+            agent.buffer.push((state, [action], reward, done_mask))
+
+            state = next_state
+            score += reward
+            # duration += 1
+            total_steps += 1
+            cnt_step += 1
+            if done:
+                break
+            if cnt_step > update_freq:
+            # if len(agent.buffer) > 1000:
+                agent.train()
+                print("Updating the Q-network")
+                cnt_step = 0
+
+        print("episode:{}, Return:{}, buffer_capacity:{}".format(episode, score, len(agent.buffer)))
+        name = env_name.split("/")[-1]
+        writer.add_scalar("dqn_%s_score" % name, score, episode)
+        # writer.add_scalar("dqn_%s_duration" % name, duration, episode)
+
+    env.close()
+
+
 def DQN_atari(env_name):
     # Generate environment
     env = gym.make(env_name, render_mode='human')
 
     # Params
-    num_episode = 1000
-    time_step = 1000
-    update_freq = 50
+    num_episode = 10000
+    update_freq = 1000
     configs = {
         "state_space": env.observation_space,
         "action_space": env.action_space,
         "replay_start_size": 1e3,
-        "buffer_size": int(1e5),
+        "buffer_size": int(1e6),
+        "target_update_freq": 1,
     }
 
     # Generate agent
@@ -93,8 +101,10 @@ def DQN_atari(env_name):
 
     # Generate tensorboard writer
     writer = tensorboard_writer(env_name)
-    train_atari(env, agent, num_episode, time_step, update_freq, writer)
+    train(env, agent, num_episode, update_freq, env_name, writer, process_atari)
 
 
 if __name__ == '__main__':
-    DQN_atari("ALE/BankHeist-v5")
+    # DQN_atari("ALE/BankHeist-v5") # far below human's performance
+    DQN_atari("ALE/VideoPinball-v5") # much better than human's performance
+    # DQN_atari("ALE/Freeway-v5") # close to human's performance
